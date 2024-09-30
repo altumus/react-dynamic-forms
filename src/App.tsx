cc73d0951ff8c55ae799e585eb386cad89d0edde
Бухарин Максим <jojojopa@icloud.com>
2024-09-30 23:48:34 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+	return (
+		<div>
+			<h1 style={{ textAlign: 'center' }}>Динамические формы</h1>
+		</div>
+	);
+}
+
+export default App;
